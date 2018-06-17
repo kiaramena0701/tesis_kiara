b904e87b5e0c81545ccec945f62b4c959ac02ab8
Kiara <karamena0701@gmail.com>
2018-06-16 22:59:49 -0400

    <@>...

--- App.js
+++ App.js
@@ -1,113 +1,73 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import Login from './src/Login';
+import Config from './src/Config';
 
-import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  AsyncStorage,
-  Platform,
-  StatusBar,
-  Alert
-} from 'react-native';
-
-import {Scene, Router, Actions } from 'react-native-router-flux';
-import Login from './src/Login'
-import Resumen from './src/Resumen'
-import TabBar from './src/TabBar'
-import HistoralSiembras from './src/HistoralSiembras'
-import HistOneSiembra from './src/HistOneSiembra'
-import ListFactorQuimico from './src/ListFactorQuimico'
-import HistoralProdAplicados from './src/HistoralProdAplicados'
+  Scene,
+  Router,
+  Actions
+} from 'react-native-router-flux';
 
 
-export default class App extends Component<{}> {
-
-   constructor(props) {
-    super(props);
-    this.state = {
-   
-    }
-  }
-  componentWillMount(){
-  }
-
-  
-
-  render() {
-    return (
-        <Router showNavigationBar={false} hideNavBar={true} 
-          gestureEnabled={false}
-          panHandlers={null}>
-
-          <Scene 
-            key="Login" 
-            component={Login}
-            title="Login"
-            hideNavBar={true}/>
-
-          <Scene 
-            key="TabBar" 
-            component={TabBar}
-            title="Resumen"
-            hideNavBar={true}/>
-
-          <Scene 
-            key="Resumen" 
-            component={Resumen}
-            title="Resumen"
-            hideNavBar={true}/>
-
-          <Scene 
-            key="HistoralSiembras" 
-            component={HistoralSiembras}
-            title="HistoralSiembras"
-            hideNavBar={false}/>
-
-           <Scene 
-            key="ListFactorQuimico" 
-            component={ListFactorQuimico}
-            title="ListFactorQuimico"
-            hideNavBar={false}/>
 
 
-            <Scene 
-            key="HistoralProdAplicados" 
-            component={HistoralProdAplicados}
-            title="HistoralProdAplicados"
-            hideNavBar={false}/>
+const reducerCreate = params => {
+  const defaultReducer = new Reducer(params);
+  return (state, action) => {
+    console.log('ACTION:', action);
+    return defaultReducer(state, action);
+  };
+};
 
-           <Scene 
-            key="HistOneSiembra" 
-            component={HistOneSiembra}
-            title="HistOneSiembra"
-            hideNavBar={false}/>
+const getSceneStyle = () => ({
+  backgroundColor: '#F5FCFF',
+  shadowOpacity: 1,
+  shadowRadius: 3,
+});
 
-       </Router>
-    );
-  }
-}
+// on Android, the URI prefix typically contains a host in addition to scheme
+const prefix = Platform.OS === 'android' ? 'mychat://mychat/' : 'mychat://';
+
+const Example = () => (
+  <Router
+    showNavigationBar={false}
+    hideNavBar={false}
+    gestureEnable={false}
+    >
+
+      <Scene
+        key="Login"
+        title="Login"
+        navBar={Login}
+        component={Login}
+        hideNavBar={false}
+        back
+      />
+
+       <Scene
+        key="Config"
+        title="Config"
+        navBar={Config}
+        component={Config}
+        hideNavBar={false}
+        back
+      />
+    
+  </Router>
+);
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    flex: 1, backgroundColor: 'transparent', justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  tabBarStyle: {
+    backgroundColor: '#eee',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  tabBarSelectedItemStyle: {
+    backgroundColor: '#ddd',
   },
 });
+
+export default Example;
--- src/Config.js
+++ src/Config.js
@@ -1,131 +1,45 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React , { Component } from 'react';
+import {View, Text, StyleSheet} from "react-native";
+import {Actions} from "react-native-router-flux";
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  Dimensions,
-  FlatList,
-  Switch,
-  TouchableHighlight,
-  TouchableOpacity,
-  Alert
-} from 'react-native';
-import { Actions } from 'react-native-router-flux';
-import TabNavigator from 'react-native-tab-navigator';
-import Login from './Login'
-import { IMG_HOME, IMG_TREE, IMG_RIGHT} from './images_b64'
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5,
+  },
+});
 
-export default class Config extends Component {
-  constructor(props) {
-    super(props);
-  
-    this.state = {
-		    switch2Value:false
-    };
-  }
-
-  toggleSwitch1(value){
-      this.setState({switch2Value: value})
-      if(value){
-        Alert.alert("Prueba de Alerta","comprobar")
-      }
-      // console.warn('Switch 1 is: ' + value)
-   }
-  componentWillMount(){
-    // console.warn('componentWillMount');
-  }
-
-  componentDidMount(){
-    // console.warn('componentDidMount');
-  }
-
-  componentDidUpdate(){
-    // AQUI EJECUTAR LAS FUNCIONES DE REFRESCAR VISTA CUANDO REGRESAS DE OTRA VISTA POR MEDIO DEL TAB-BAR
-    // console.warn('componentDidUpdate');
-  }
+export default class  Config extends Component {
+  static onEnter = () => {
+    Actions.refresh({
+      title: 'Login!',
+      rightTitle: 'rightTitle',
+      onRight: () => {},
+    });
+  };
 
   render() {
+    const title = this.props.title || 'No Title';
+    const data = this.props.data || 'No Data';
+    console.log("Login RENDER");
     return (
-      <View style={styles.container}>
-        <View  style={styles.container2}>
-          	<Text style={{padding:8}}>Activar Notificaciones</Text>
-            <Switch
-              onValueChange = { (value) => { this.toggleSwitch1(value) }}
-              value = {this.state.switch2Value}/>
-        </View>
-
-        <View  style={{flexDirection:'column',alignItems: 'stretch'}}>
-          <TouchableHighlight>
-            <Text style={styles.instructions}> + (Añadir Productos Quimicos)</Text>
-          </TouchableHighlight>
-          <TouchableHighlight>
-            <Text style={styles.instructions}> + (Añadir Factor Siembra)</Text>
-          </TouchableHighlight>
-        </View>
-       
+      <View style={[styles.container, this.props.style]}>
+        <TouchableHighlight onPress={()=>{Actions.Login()}}>
+          <Text>Login page 1</Text>
+        </TouchableHighlight>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create ({
-  container: {
-      flexDirection: 'column',
-      padding: 8,
-      // width: 332,
-      marginTop: 75,
-      // backgroundColor: '#d9f9b1',
-      alignItems: 'center',
-  },
-
-  container2: {
-    padding: 8,
-    flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center',
-  },
-  text: {
-    fontSize: 18,
-    fontWeight:'700'
-  },
-  text2: {
-      fontSize: 14,
-      fontWeight:'700'
-   },
-  item_jugada: {
-  	flexDirection: 'row', 
-    paddingRight:4,
-    paddingLeft:4,
-    // paddingTop: 10,
-    paddingBottom: 10,
-    marginTop: 3,
-    alignItems: 'flex-start',
-    // borderBottomWidth: 1
-  },
-  item:{
-  	flexDirection:'row',
-  	alignItems:'stretch',
-  	backgroundColor: '#f1f8e9',
-  	paddingLeft:16,
-  	paddingTop:8,
-  	paddingBottom:8,
-  	borderWidth: 1,
-  	borderColor: '#c5e1a5',
-  	borderRadius:8
-  },
-  Jugadas: {
-     
-    // flex:2,
-    // justifyContent: 'center',
-    // alignItems: 'stretch',
-    // flexDirection:  'row',
-   }
-})
